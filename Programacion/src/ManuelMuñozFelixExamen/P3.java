f9b5813e579be21dd3244def789ea179ce0548d5
ManuelMF <103216638+ManuelMF@users.noreply.github.com>
2022-04-07 21:51:12 +0200

    Delete Programacion directory

--- Programacion/src/ManuelMuñozFelixExamen/P3.java
+++
@@ -1,9 +0,0 @@
-package ManuelMuñozFelixExamen;
-
-public class P3 {
-
-	//pregunta 3
-	
-	//saldra por pantalla 15 
-
-}
--- Programacion/src/tren_en_raya.java
+++
@@ -1,9 +0,0 @@
-
-public class tren_en_raya {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
